cd643267e53a4125ad0afa2b739ba5373b959942
Pietro <pietro.aquino@tce.ma.gov.br>
2023-01-12 10:58:01 -0300

    .

--- src/Row.js
+++ src/Row.js
@@ -21,7 +21,10 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             <h2 className={"row__title"}>{title}</h2>
             <div className={`row__posters`}>
                 {movies.map(movie => (
-                    <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
+                    ((isLargeRow && movie.poster_path) ||
+                    (!isLargeRow && movie.backdrop_path)) && (
+                        <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
+                    )
                 ))}
             </div>
         </div>
